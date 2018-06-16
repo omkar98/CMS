c53473570e7c0ed515932e6eb7f24ef0ab16118c
omkar98 <edu.omkar@gmail.com>
2018-06-16 18:22:49 +0530

    Dynamically created Posts

--- includes/posts.php
+++ includes/posts.php
@@ -1,27 +1,44 @@
-<h1 class="page-header">
-                    Page Heading
-                    <small>Secondary Text</small>
-                </h1>
-                <!-- First Blog Post -->
-                <h2>
-                    <a href="#">Blog Post Title</a>
-                </h2>
-                <p class="lead">
-                    by <a href="index.php">Start Bootstrap</a>
-                </p>
-                <p><span class="glyphicon glyphicon-time"></span> Posted on August 28, 2013 at 10:00 PM</p>
-                <hr>
-                <img class="img-responsive" src="http://placehold.it/900x300" alt="">
-                <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore, veritatis, tempora, necessitatibus inventore nisi quam quia repellat ut tempore laborum possimus eum dicta id animi corrupti debitis ipsum officiis rerum.</p>
-                <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
-                <hr>
-                <!-- Pager -->
-                <ul class="pager">
-                    <li class="previous">
-                        <a href="#">&larr; Older</a>
-                    </li>
-                    <li class="next">
-                        <a href="#">Newer &rarr;</a>
-                    </li>
-                </ul>
+<?php
+
+$query = "SELECT * FROM posts";
+$result = mysqli_query($connection, $query); 
+$row = mysqli_fetch_assoc($result);
+while($row)
+{
+    $post_tite=row['post_tile'];
+    $post_author=row['post_author'];
+    $post_date=row['post_date'];
+    $post_image=row['post_image'];
+    $post_content=row['post_content'];
+?>
+        <h1 class="page-header">
+            Page Heading
+            <small>Secondary Text</small>
+        </h1>
+        <!-- First Blog Post -->
+        <h2>
+            <a href="#"><?php $post_tite ?></a>
+        </h2>
+        <p class="lead">
+            by <a href="index.php"><?php $post_author ?></a>
+        </p>
+        <p><span class="glyphicon glyphicon-time"></span> Posted on <?php $post_date ?></p>
+        <hr>
+        <img class="img-responsive" src="http://placehold.it/900x300" alt="">
+        <hr>
+        <p><?php $post_content ?></p>
+        <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
+        <hr>
+        <!-- Pager -->
+        <ul class="pager">
+            <li class="previous">
+                <a href="#">&larr; Older</a>
+            </li>
+            <li class="next">
+                <a href="#">Newer &rarr;</a>
+            </li>
+        </ul>
+<?php
+    $row=mysqli_fetch_assoc($result);
+}
+?>
