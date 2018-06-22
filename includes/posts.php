c9e42acb9922eec23a9210b2587799f2c9adac6a
omkar98 <edu.omkar@gmail.com>
2018-06-22 16:12:14 +0530

    Posts Final Update

--- includes/posts.php
+++ includes/posts.php
@@ -7,6 +7,7 @@ $result = mysqli_query($connection, $query);
 $row = mysqli_fetch_assoc($result);
 while($row)
 {
+    $p_id=$row['post_id'];
     $post_title=$row['post_title'];
     $post_author=$row['post_author'];
     $post_date=$row['post_date'];
@@ -19,16 +20,16 @@ while($row)
         </h1>
         <!-- First Blog Post -->
         <h2>
-            <a href="#"><?php echo $post_title ?></a>
+            <a href="post.php?source=<?php echo $p_id; ?>"><?php echo $post_title; ?></a>
         </h2>
         <p class="lead">
-            by <a href="index.php"><?php echo $post_author ?></a>
+            by <a href="index.php"><?php echo $post_author; ?></a>
         </p>
-        <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date ?></p>
+        <p><span class="glyphicon glyphicon-time"></span> Posted on <?php echo $post_date; ?></p>
         <hr>
         <img class="img-responsive" src="images/<?php echo $post_image; ?>" alt="">
         <hr>
-        <p><?php echo $post_content ?></p>
+        <p><?php echo $post_content; ?></p>
         <a class="btn btn-primary" href="#">Read More <span class="glyphicon glyphicon-chevron-right"></span></a>
         <hr>
 <?php
